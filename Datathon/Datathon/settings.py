64f09a3a50c7e8c5580cc8c26cba0fc0be751e55
SHYAM-001 <shyamkathir007@gmail.com>
2024-05-19 16:27:56 +0530

    updated the settings.py

--- Datathon/Datathon/settings.py
+++ Datathon/Datathon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_9o#sil2lu6rj5@7zny3+*5%lqqfiz!9%9dxyldl_2xl&cj1^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.171.136.249']
 
 
 # Application definition
